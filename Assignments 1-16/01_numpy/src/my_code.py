5aa071b5a3e6d5047cfdd380ca09d1b296ac9774
Nihilistic-Saint <76147955+Nihilistic-Saint@users.noreply.github.com>
2022-02-22 11:38:29 +0200

    01_submit

--- Assignments 1-16/01_numpy/src/my_code.py
+++ Assignments 1-16/01_numpy/src/my_code.py
@@ -1,2 +1,9 @@
 import numpy as np
+from numpy import save
+# Luo taulukko a, jossa on 3 rivi¨a ja 4 saraketta ja kaikkien
+# alkioiden arvo on 0. Talleta taulukko tiedostoon a.npy.
 
+a = np.zeros((3,4))
+print(a)
+
+save('a.npy',a)
